09fca105445d4c91ca05963017d9e51044366016
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-15 15:55:08 +0900

    HttpServletRequest, Response

--- src/main/java/com/ll/sb20231114/domain/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/controller/ArticleController.java
@@ -1,12 +1,17 @@
 package com.ll.sb20231114.domain.article.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.sb20231114.domain.article.entity.Article;
 import com.ll.sb20231114.domain.article.service.ArticleService;
+import com.ll.sb20231114.global.Rq;
 import com.ll.sb20231114.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class ArticleController {
     private final ArticleService articleService;
+    private final Rq rq;
 
     @GetMapping("/article/write")
     String showWrite() {
@@ -39,9 +45,10 @@ public class ArticleController {
     }
 
     @PostMapping("/article/write2")
-    @ResponseBody
-    RsData write2(
-            HttpServletRequest req
+    @SneakyThrows
+    void write2(
+            HttpServletRequest req,
+            HttpServletResponse resp
     ) {
         String title = req.getParameter("title");
         String body = req.getParameter("body");
@@ -54,7 +61,11 @@ public class ArticleController {
                 article
         );
 
-        return rs;
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().println(objectMapper.writeValueAsString(rs));
     }
 
     @GetMapping("/article/getLastArticle")
@@ -67,5 +78,35 @@ public class ArticleController {
     @ResponseBody
     List<Article> getArticles() {
         return articleService.findAll();
+    }
+
+    @GetMapping("/article/articleServicePointer")
+    @ResponseBody
+    ArticleService articleServicePointer() {
+        return articleService;
+    }
+
+    @GetMapping("/article/httpServletRequestPointer")
+    @ResponseBody
+    String httpServletRequestPointer(HttpServletRequest req) {
+        return req.toString();
+    }
+
+    @GetMapping("/article/httpServletResponsePointer")
+    @ResponseBody
+    String httpServletRequestPointer(HttpServletResponse resq) {
+        return resq.toString();
+    }
+
+    @GetMapping("/article/rqPointer")
+    @ResponseBody
+    String rqPointer(Rq rq) {
+        return rq.toString();
+    }
+
+    @GetMapping("/article/rqTest")
+    String showRqTest(Model model) {
+        model.addAttribute("rq", rq.toString());
+        return "/article/rqTest";
     }
 }
