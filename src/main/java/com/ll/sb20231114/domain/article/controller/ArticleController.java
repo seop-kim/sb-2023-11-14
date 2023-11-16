304724c2c6e64284e2140e1e8c5944c180e0007f
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-16 22:16:47 +0900

    refactor: Organize code

--- src/main/java/com/ll/sb20231114/domain/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/controller/ArticleController.java
@@ -2,9 +2,6 @@ package com.ll.sb20231114.domain.article.controller;
 
 import com.ll.sb20231114.domain.article.entity.Article;
 import com.ll.sb20231114.domain.article.service.ArticleService;
-import com.ll.sb20231114.global.Rq;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,13 +14,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class ArticleController {
     private final ArticleService articleService;
-    private final Rq rq;
 
     // write form
     @GetMapping("/article/write")
@@ -68,6 +63,7 @@ public class ArticleController {
         return "redirect:/article/list?msg=" + msg;
     }
 
+    // modify form
     @GetMapping("/article/modify/{id}")
     String modifyForm(@PathVariable Long id, Model model) {
         Optional<Article> findOne = articleService.findById(id);
@@ -76,54 +72,12 @@ public class ArticleController {
         return "/article/modify";
     }
 
+    // modify
     @PostMapping("/article/modify")
     String modify(@Valid ModifyForm form) {
         articleService.modify(form);
         String msg = "article modify";
         return "redirect:/article/detail/%d?msg=".formatted(form.getId()) + msg;
-    }
-
-
-    @GetMapping("/article/getLastArticle")
-    @ResponseBody
-    Article getLastArticle() {
-        return articleService.findLastArticle();
-    }
-
-    @GetMapping("/article/getArticles")
-    @ResponseBody
-    List<Article> getArticles() {
-        return articleService.findAll();
-    }
-
-    @GetMapping("/article/articleServicePointer")
-    @ResponseBody
-    ArticleService articleServicePointer() {
-        return articleService;
-    }
-
-    @GetMapping("/article/httpServletRequestPointer")
-    @ResponseBody
-    String httpServletRequestPointer(HttpServletRequest req) {
-        return req.toString();
-    }
-
-    @GetMapping("/article/httpServletResponsePointer")
-    @ResponseBody
-    String httpServletRequestPointer(HttpServletResponse resq) {
-        return resq.toString();
-    }
-
-    @GetMapping("/article/rqPointer")
-    @ResponseBody
-    String rqPointer(Rq rq) {
-        return rq.toString();
-    }
-
-    @GetMapping("/article/rqTest")
-    String showRqTest(Model model) {
-        model.addAttribute("rq", rq.toString());
-        return "/article/rqTest";
     }
 
     @Data
