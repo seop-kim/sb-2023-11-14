a61a505fcbdb2e6ae1146bec15e3f795bf570e59
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-21 09:45:18 +0900

    feat: Add article test data

--- src/main/java/com/ll/sb20231114/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/repository/ArticleRepository.java
@@ -1,6 +1,9 @@
 package com.ll.sb20231114.domain.article.repository;
 
 import com.ll.sb20231114.domain.article.entity.Article;
+import com.ll.sb20231114.domain.member.entity.Member;
+import com.ll.sb20231114.domain.member.repository.MemberRepository;
+import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +13,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class ArticleRepository {
+    private final MemberRepository memberRepository;
     private final List<Article> articles = new ArrayList<>();
+
+    @PostConstruct // 생성자가 생성된 뒤에 실행된다.
+    void init() {
+        Member member1 = memberRepository.findById(1L).get();
+        Member member2 = memberRepository.findById(2L).get();
+
+        save(new Article(member1, "test1", "test1"));
+        save(new Article(member2, "test2", "test2"));
+        save(new Article(member2, "test3", "test3"));
+    }
 
     public Article save(Article article) {
         if (article.getId() == null) {
