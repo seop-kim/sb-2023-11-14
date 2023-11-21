8f72cd157682d9cd4fcbb3cd85f0f738033d0af7
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-21 13:48:16 +0900

    feat: Add SecurityConfig

--- src/main/java/com/ll/sb20231114/domain/home/controller/HomeController.java
+++ src/main/java/com/ll/sb20231114/domain/home/controller/HomeController.java
@@ -1,12 +1,30 @@
 package com.ll.sb20231114.domain.home.controller;
 
+import jakarta.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String goArticleList() {
         return "redirect:/article/list";
+    }
+
+
+    @GetMapping("/home/session")
+    @ResponseBody
+    public Map<String, Object> showSession(HttpSession session) {
+        return Collections.list(session.getAttributeNames()).stream()
+                .collect(
+                        Collectors.toMap(
+                                key -> key,
+                                key -> session.getAttribute(key)
+                        )
+                );
     }
 }
---
+++ src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.ll.sb20231114.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+        http.csrf(
+                csrf -> csrf.disable()
+        );
+
+        return http.build();
+    }
+}
