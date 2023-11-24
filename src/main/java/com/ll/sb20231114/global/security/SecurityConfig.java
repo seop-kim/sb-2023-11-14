e46edf2431a8e9688076e65861d36244fdc8da1a
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-24 13:40:43 +0900

    feat: Add h2 security option

--- src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 public class SecurityConfig {
@@ -13,6 +14,16 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(
+                        csrf -> csrf
+                                .ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers(
+                        headers -> headers
+                                .addHeaderWriter(
+                                        new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)
+                                )
+                )
                 .authorizeHttpRequests(
                         authorizeHttpRequests -> authorizeHttpRequests
                                 .requestMatchers("/adm/**")
