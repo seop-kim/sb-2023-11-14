8276e3dff19cf00917bba9bab1fd5a0be79fa118
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-24 13:46:39 +0900

    feat: Add attr column

--- src/main/java/com/ll/sb20231114/domain/base/arrt/entity/Attr.java
+++ src/main/java/com/ll/sb20231114/domain/base/arrt/entity/Attr.java
@@ -2,9 +2,17 @@ package com.ll.sb20231114.domain.base.arrt.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Attr {
     @Id
     private Long id;
+
+    private String name;
+
+    private String val;
 }
