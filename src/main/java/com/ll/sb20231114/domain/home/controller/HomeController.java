e74f11a98c3c04c35323d0f8ba13b32ae2a042cf
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-24 11:09:00 +0900

    refactor: Apply rq

--- src/main/java/com/ll/sb20231114/domain/home/controller/HomeController.java
+++ src/main/java/com/ll/sb20231114/domain/home/controller/HomeController.java
@@ -1,18 +1,22 @@
 package com.ll.sb20231114.domain.home.controller;
 
+import com.ll.sb20231114.global.rq.Rq;
 import jakarta.servlet.http.HttpSession;
 import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final Rq rq;
     @GetMapping("/")
-    public String goArticleList() {
-        return "redirect:/article/list";
+    public String goToArticleList(String msg) {
+        return rq.redirect("/article/list", msg);
     }
 
 
