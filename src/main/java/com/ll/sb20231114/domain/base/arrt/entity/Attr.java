f9feadfafed85aaa824e730397040de53f30f200
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-24 13:40:53 +0900

    feat: Add attr

---
+++ src/main/java/com/ll/sb20231114/domain/base/arrt/entity/Attr.java
@@ -0,0 +1,10 @@
+package com.ll.sb20231114.domain.base.arrt.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Attr {
+    @Id
+    private Long id;
+}
